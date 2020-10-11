36ec475ea9ef0de7e3e951edaf3b85f73dbe0a21
Vasile Furdui <vasiafurdui@yahoo.com>
2020-10-11 21:54:53 +0300

    added icons to components

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { faFacebook, faInstagram, faTumblr, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faTumblr = faTumblr;
+  faTwitter = faTwitter;
+}
